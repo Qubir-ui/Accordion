e4c9001040c699f92548ac8558ed69f1ba5911ee
Qubir-ui <qubir29@yandex.ru>
2023-04-01 18:09:13 +0300

    visual fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // const accordion = document.querySelector("#accordion")
-    // const accordionElement = document.querySelectorAll("#accordion .group")
-    // accordion.firstChild.childNodes
-    // accordionElement.forEach((el) => {
-    //     el.addEventListener("click", ()=>{
-    //
-    //     })
-    // })
+    const arrows = document.querySelectorAll(".arrow")
+    const accordionElement = document.querySelectorAll("#accordion .group")
+
+    accordionElement.forEach((el) => {
+        el.addEventListener("click", (e) => {
+            arrows.forEach((arrow) => {
+                if (el.ariaExpanded === "true" && arrow === el.parentNode.lastElementChild) {
+                    arrow.style.transform = 'rotate(' + 180 + 'deg)'
+                } else {
+                    arrow.style.transform = 'rotate(' + 0 + 'deg)'
+                }
+            })
+            if (el.ariaExpanded === "true") {
+                el.parentElement.style.border = '1px solid #636C7C'
+            } else {
+                el.parentElement.style.border = '1px solid #C6CDDA'
+            }
+        })
+    })
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: ["./index.html", "./node_modules/tw-elements/dist/js/**/*.js"],
     theme: {
         extend: {
+            borderColor: ['checked'],
             screens: {
                 '1_xl': '1360px',
             },
